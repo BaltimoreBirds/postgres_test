61b7ff8d97edb7c092bb88bb7fa1f4b881aca418
Michael Swanson <mswanson@terpmail.umd.edu>
2014-04-05 17:24:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostgresTest::Application.config.secret_key_base = '6d3d7061229ecd9c6257a0cb0b2415c41885275cbfe46ac2bc4937500675be9904f35c7a57c667984baeeeffde40b41eba1c8ccbdbd56ef1fd9c08dcf4b71937'
